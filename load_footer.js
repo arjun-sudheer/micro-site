8fbf76336c6fe9624a7628014041536e0cebae40
arjun-sudheer <133593123+arjun-sudheer@users.noreply.github.com>
2023-08-07 22:09:20 -0400

    Add files via upload

---
+++ load_footer.js
@@ -0,0 +1,4 @@
+document.getElementById("myFooter").innerHTML =
+	"<a href='index.html'>Home</a> | <a href='product.html'>Product Page</a> | <a href='company.html'>About Company</a> | <a href='contact.html'>Contact Us</a> </p>"
+	+"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
+	+ " rights reserved.</p>"
